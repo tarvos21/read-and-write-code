87dd66badaa949a12a184044a1f9da70058a72b3
DuBing <tarvos21@live.com>
2017-01-05 18:36:29 +0800

    grep -Fxvf, diff two files and output

---
+++ grep-Fxvf.sh
@@ -0,0 +1,4 @@
+grep -Fxvf original-variables.less paper/variables.less >> paper-diff.txt
+
+# This will output the different lines of paper/variables.less to txt file
+# Useful when want ot diff two files
