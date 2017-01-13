37205b5e268eead85e848d5ae2c0b6db1c9b06c5
DuBing <tarvos21@live.com>
2017-01-13 21:29:44 +0800

    shell script to make directory and move files into them

---
+++ md.sh
@@ -0,0 +1,5 @@
+mkdir collections globals elements modules
+mv site* globals
+mv {button*,container*,divider*,flag*,header*,icon*,image*,input*,label*,list*,loader*,rail*,reveal*,segment*,step*} elements
+mv {breadcrumb*,form*,grid*,menu*,message*,table*} collections
+mv {accordion*,checkbox*,dimmer*,dropdown*,embed*,modal*,nag*,popup*,progress*,rating*,search*,shape*,sidebar*,sticky*,tab*,transition*} modules
