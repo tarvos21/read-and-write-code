6217f1da4fab5997262861f05059144822b4bed5
Du Bing <tarvos21@gmail.com>
2017-02-07 00:11:06 +0800

    3 kinds of quote: `_` , "_" , '_', back, double, single

---
+++ back-single-double-quote.sh
@@ -0,0 +1,8 @@
+echo `date`    # output time
+echo "`date`"    # output time
+echo '`date`'   # output `date`
+
+# back quote `_` return the contents of the command
+# double quote "_" is week quote, can expand
+# single quote '_' is strong quote, no substitution, just raw print
+
