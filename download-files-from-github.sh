c7d5cbe20800ef5800e6d437d45d75fcd16bbc59
Du Bing <tarvos21@gmail.com>
2017-02-08 23:17:35 +0800

    curl -o filename URL(raw)

--- download-files-from-github.sh
+++ download-files-from-github.sh
@@ -1,5 +1,7 @@
 1. Click the "Raw" button on the file page
 2. Copy the URL in the Browser
-3. curl URL -O filename
+3. curl URL -o filename
+
 
 # curl will get the content and save it to "filename"
+# -O to save the original name
