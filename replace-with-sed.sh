78e1cb0444e1fe6f0949adc21829f8baf209762f
Du Bing <tarvos21@gmail.com>
2017-02-09 14:26:20 +0800

    grep -rl "foo" | xargs sed -i "s/foo/bar/g"

--- replace-with-sed.sh
+++ replace-with-sed.sh
@@ -1,3 +1,8 @@
 sed -i -- 's/foo/bar/g' *
 
 # this will replace every "foo" in all files of current directory with "bar"
+
+grep -rl '.btn-shadow' . | xargs sed -i 's/.btn-shadow/.button-shadow/g'
+
+# this will replace every ".btn-shadow" with ".button-shadow" recursively in current directory
+
